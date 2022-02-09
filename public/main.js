d8c3c7bad288d358ed1d0de9ce60063438a8f4fb
Lukas Nesittka <97050688+LukasNesittka@users.noreply.github.com>
2022-02-09 15:24:01 -0600

    Sanic

--- public/main.js
+++ public/main.js
@@ -4,6 +4,11 @@ const boredBtn = (evt) => {
     evt.preventDefault()
     axios
         .get('http://localhost:4000/api/activity')
+        .then(res => {
+            console.log(res.data)
+            const { activity } = res.data
+            document.querySelector('.activity').textContent = activity
+        })
 }
 
 document.querySelector(".bored").addEventListener('click',boredBtn)
--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const path = require('path')
+const { default: axios } = require('axios')
 
 const app = express()
 
@@ -10,6 +11,15 @@ app.use(express.static('public'))
 
 app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname,'../public/index.html'))
+})
+app.get('/api/activity/', async(req,res) => {
+    let activity;
+    await axios
+        .get('https://www.boredapi.com/api/activity/')
+        .then(response => {
+            activity = response.data
+        })
+        res.status(200).send(activity)
 })
 
 const port = process.env.PORT || 4000
