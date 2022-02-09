6cb299ba429faaf6a18283160029d92b557f72b6
Lukas Nesittka <97050688+LukasNesittka@users.noreply.github.com>
2022-02-09 15:38:27 -0600

    Changes

--- public/main.js
+++ public/main.js
@@ -3,7 +3,7 @@ let bored = document.querySelector(".bored")
 const boredBtn = (evt) => {
     evt.preventDefault()
     axios
-        .get('http://localhost:4000/api/activity')
+        .get('/api/activity')
         .then(res => {
             console.log(res.data)
             const { activity } = res.data
